61e0c2d7e177fb326f6bdc514a50e95a8227aedd
Hanna.Lytvynenko <ani-k-a@ukr.net>
2023-12-28 00:14:51 +0100

    add star rating and apartment item

--- src/components/DirectivesTest.vue
+++ src/components/DirectivesTest.vue
@@ -1,9 +1,27 @@
 <template>
-  <div></div>
+  <div v-if="isVisible">
+    <h1>Directives 1</h1>
+  </div>
+  <div v-show="isVisible">
+    <h1>Directives 2</h1>
+  </div>
+  <ul>
+    <template v-for="fruit in fruits">
+      <li v-if="fruit !== 'banana'" :key="fruit">{{ fruit }}</li>
+    </template>
+  </ul>
 </template>
 
 <script>
-export default {};
+export default {
+  name: "DirectivesTest",
+  data() {
+    return {
+      isVisible: false,
+      fruits: ["kivi", "mango", "banana"],
+    };
+  },
+};
 </script>
 
 <style lang="scss" scoped></style>
