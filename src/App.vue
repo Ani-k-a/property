e95fe177bdbb955e45120914a23eaf2e6ff19e6a
Hanna.Lytvynenko <ani-k-a@ukr.net>
2023-12-27 17:51:53 +0100

    test commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,45 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <OrderItem msg="Welcome to Your Vue.js App" />
+    <OrderList />
+    <Button @click="increment"> Click me </Button>
+    <h1>{{ title }}</h1>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
+import OrderItem from "./components/OrderItem.vue";
+import OrderList from "./components/OrderList.vue";
+
+import Button from "./components/Button.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+    OrderItem,
+    OrderList,
+    Button,
+  },
+  data() {
+    return {
+      amountClicks: 0,
+    };
+  },
+  computed: {
+    title() {
+      return `amoumt of clicks ${this.amountClicks}`;
+    },
+  },
+  methods: {
+    increment() {
+      this.amountClicks += 1;
+    },
+  },
+};
 </script>
 
 <style>
---
+++ src/components/OrderList.vue
@@ -0,0 +1,20 @@
+<template id="OrderList">
+  <h1>Заказы</h1>
+  <ul>
+    <li>
+      {{ products }}
+    </li>
+  </ul>
+</template>
+
+<script>
+// const OrderList = new Vue({
+//   el: "#OrderList",
+//   data: {
+//     products: "ugui",
+//   },
+// });
+export default {
+  name: "OrderList",
+};
+</script>
